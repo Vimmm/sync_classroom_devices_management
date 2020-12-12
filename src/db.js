ac53fa359281ece9a0e473b54d43e5fbdd035ae5
Vimmm <1366645324@qq.com>
2020-12-12 16:47:33 +0800

    password

--- src/db.js
+++ src/db.js
@@ -3,7 +3,8 @@ const mysql = require('mysql')
 const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'XiaoShuai@207',
+    // password: 'XiaoShuai@207',
+    password: '',
     port: '3306',
     database: 'sc_devices'
 })
